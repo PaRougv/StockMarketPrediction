1d3c54dca967976b9ef1ca53ca465d21f28a6a4a
Shaving Foam <paragvastrad37@gmail.com>
2025-06-18 20:29:31 +0530

    csv conversion and plotting of the data on candlesticks

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas_datareader as data
 import yfinance as yf
 import datetime as dt
+import plotly.graph_objects as go
 
 plt.style.use('fivethirtyeight')
 
@@ -14,3 +15,20 @@ start = dt.datetime(2000 , 1 , 1)
 end = dt.datetime(2025 , 6 , 18)
 
 df = yf.download(stock , start , end)
+#print(df.head())
+#print(df.tail())
+
+print(df.shape)
+
+print(df.info())
+
+print(df.describe())
+df = df.reset_index()
+
+df.to_csv("powergrid.csv")
+data01 = pd.read_csv("powergrid.csv")
+
+
+fig = go.Figure(data=[go.Candlestick(x = data01['Date'], open = data01['Open'], high = data01['High'], low = data01['Low'], close = data01['Close'])])
+fig.update_layout(xaxis_rangeslider_visible = False)
+fig.show()
