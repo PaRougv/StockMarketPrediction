cb04d0098b3171b3fb6290cf1fdbbdaddf0d2804
Shaving Foam <paragvastrad37@gmail.com>
2025-06-18 19:24:42 +0530

    initialization of modules and requirements at the start

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+
+plt.style.use('fivethirtyeight')
+
+# No need for %matplotlib inline in .py files
